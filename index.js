1d5080ef4560b934fded3bc66bf8eab3d1d61369
Admas Molla <amolla@whitepages.com>
2022-08-31 14:04:14 -0700

    got to a working version

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // https://whitepages.atlassian.net/rest/api/latest/issue/PREM-4406/changelog
 
 
-const axios = require('axios');
-const testIssue = require('./testdoc.json')
 require('dotenv').config()
+const axios = require('axios')
+const Promise = require('bluebird')
 
 let config = {
     headers: {
@@ -24,9 +24,11 @@ const getEpicStories = (epicId) => {
     return instance
     .get(`https://whitepages.atlassian.net/rest/api/2/search?jql=${encodeURIComponent(`"Epic Link" = ${epicId}`)}`)
     .then(res => {
-      return res.data.issues.map((k) => {
-          return {key:k.key}
+      const stories = res.data.issues.map((k) => {
+          return k.key
       });
+      console.log(`found ${stories.length} child stories`)
+      return stories
     })
     .catch(error => {
       console.error(error);
@@ -36,65 +38,88 @@ const getEpicStories = (epicId) => {
 const findStory = (issue) => {
     return instance
     .get(`https://whitepages.atlassian.net/rest/api/latest/issue/${issue}`)
+    .then(r => r.data.fields)
 }
 
-const getStoryDetails = (story, issue) => {
-    console.log("story", story.fields.summary)
-    console.log("assignee",story.fields.assignee.displayName)
-    console.log("status", story.fields.status.statusCategory.name, story.fields.status.name)
-    console.log("estimate", story.fields.timetracking.originalEstimate)
-    console.log("comments", story.fields.comment.comments.length)
-    instance
-        .get(`https://whitepages.atlassian.net/rest/api/latest/issue/${issue}/changelog`)
-        .then(r => {
-            const inProgress = findInterestingInfo(r.data.values, {"toString": "In Progress"})
-            const done = findInterestingInfo(r.data.values, {"toString": "Done"})
-
-            return  {inProgress, done}
-        })
-        .then(({inProgress, done}) => {
-            if(inProgress.length > 0){
-                const startDate = new Date(inProgress[0].created)
-                let end = Date.now()
-                if(done.length > 0){
-                    end = new Date(done[0].created)
-                }
-                console.log("actual days", getBusinessDatesCount(startDate,end))
-            } else {
-                console.log("actual days", "not started")
+const getStoryDetails = (issue) => {
+    const storyDetails = findStory(issue)
+        .then(story => {
+            return {
+                "story": story.summary,
+                "assignee":story.assignee?.displayName || "none",
+                "status": story.status.statusCategory.name +" "+ story.status.name,
+                "estimate": story.timetracking.originalEstimate,
+                "comments": story.comment.comments.length
             }
-
         })
-        .catch(e => {
-            console.error('there was an error fetching an issues changelong', issue, e)
+
+    const storyHistory =
+        instance
+            .get(`https://whitepages.atlassian.net/rest/api/latest/issue/${issue}/changelog`)
+            .then(r => {
+                const inProgress = findInterestingInfo(r.data.values, {"toString": "In Progress"})
+                const done = findInterestingInfo(r.data.values, {"toString": "Done"})
+
+                return  {inProgress, done}
+            })
+            .then(({inProgress, done}) => {
+                if(inProgress.length > 0){
+                    const startDate = new Date(inProgress[0].created)
+                    let end = Date.now()
+                    if(done.length > 0){
+                        end = new Date(done[0].created)
+                    }
+                    return {"actual days": getBusinessDatesCount(startDate,end)}
+                } else {
+                    return {"actual days": "not started"}
+                }
+            })
+            .catch(e => {
+                console.error('there was an error fetching an issues changelong', issue, e)
+            })
+    
+    return Promise.all([storyHistory,storyDetails])
+        .then(arr => {
+            return {
+                ...arr[0],
+                ...arr[1],
+            }
         })
 }
 
-findStory(issue)
-    .then(r => {
-        if(r.data.fields.issuetype.name === 'Epic'){
-            return getEpicStories(issue)
-                .then((stories) => {
-                    stories.forEach((story) => {
-                        findStory(story.key)
-                        .catch(e => {
-                            console.error("error getting child stories",e)
+const recurse = (story) => {
+    return findStory(story)
+        .then(r => {
+            if(r.issuetype.name === 'Epic'){
+                return getEpicStories(story)
+                    .then((substories) => {
+                        const childStories = substories.map((substory) => {
+                            return recurse(substory)
+                        })
+                        return Promise.all(childStories)
+                        .then((e) =>{
+                            console.log(e)
                         })
                     })
-                })
-                .catch(e => {
-                    console.error("error getting child stories",e)
-                })
-        } else {
-            console.log("getting story details", issue)
-            getStoryDetails(r.data,issue)
-        }
-    })
-    .catch(error => {
-        console.error('error fetching issue details', issue, error)
-    })
+                    .catch(e => {
+                        console.error("error getting child stories",e)
+                    })
+            } else {
+                console.log("getting story details", story)
+                return getStoryDetails(story)
+            }
+        })
+        .catch(error => {
+            if(error.response.status === 404) {
+                console.log('error, 404')
+            } else {
+                console.error('error fetching issue details', story, error)
+            }
+        })
+}
 
-
+recurse(issue)
+    .then(console.log)
 
 const findInterestingInfo = (changeLogValues, objToFind) => {
     const keysToSearch = Object.keys(objToFind)
